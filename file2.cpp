046388b9440e8aad7e4e6b5d911e1346e18fd803
Evelofdex <evelofdex@gmail.com>
2026-03-03 13:26:32 +0700

    menambahkan fungsi dan prosedur

--- file2.cpp
+++ file2.cpp
@@ -10,6 +10,19 @@ void input() {
     cout << "masukkan lebar =";
     cin >> l;
 }
+float jumlah(float a, float b,float c) {
+    return a + b + c;
+}
+
+int luaspersegiP(int a, int b){
+    return a * b;
+}
+
+void output(){
+    cout << "hasilnya =" << luaspersegiP(p, 1) << endl;
+    cout << "terima kasih" << endl;
+}
+
 int main() {
 
 }
