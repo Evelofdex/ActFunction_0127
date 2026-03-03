39fd0116355a601b0a03ae418b18249d2878d63e
Evelofdex <evelofdex@gmail.com>
2026-03-03 13:18:35 +0700

    menambah prosedur

--- file1.cpp
+++ file1.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 // deklarasi global
 int p, l, luas;
+//implementasi prosedur dan fungsi
+void input() {
+    cout << "masukan panjang =";
+    cin >> p;
+    cout << "masukan lebar =";
+    cin >> l;
+}
 
 int main(){
 
