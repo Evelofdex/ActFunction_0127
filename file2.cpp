0fa546fe8197621d0a7e3b2e17df5e7d1b9998ff
Evelofdex <evelofdex@gmail.com>
2026-03-03 13:24:21 +0700

    implementasi prosedur dan fungsi

--- file2.cpp
+++ file2.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 //deklarasi global
 int p, l;
-
+//implementasi prosedur dan fungsi
+void input() {
+    cout << "masukkan panjang =";
+    cin >> p;
+    cout << "masukkan lebar =";
+    cin >> l;
+}
 int main() {
 
 }
